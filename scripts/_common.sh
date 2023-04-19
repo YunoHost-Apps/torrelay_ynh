efea5d98b310c105810f1cd84337d5a7a02500b7
OniriCorpe <oniricorpe@disroot.org>
2023-04-19 21:15:06 +0200

    adding tor repos to always get the latest versions

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,8 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="tor obfs4proxy"
+pkg_dependencies="apt-transport-https obfs4proxy"
+pkg_dependencies_extra="tor tor-geoipdb deb.torproject.org-keyring"
 
 #=================================================
 # PERSONAL HELPERS
